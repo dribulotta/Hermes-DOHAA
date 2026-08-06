a0f1ae3a0c0ed8c9af594ecb7a626b2b0a2fc728
dribulotta <dribulotta@gmail.com>
2026-08-06 05:28:35 -0300

    test: use documentation address in URL normalization

--- tests/test_hermes_api_url.py
+++ tests/test_hermes_api_url.py
@@ -6,15 +6,15 @@ from hermes_dohaa.runtime.hermes_api import HermesApiRuntime
 class HermesApiUrlTests(unittest.TestCase):
     def test_chat_completions_url_accepts_root_and_v1_forms(self):
         expected = (
-            "http://10.10.10.106:8642"
+            "http://192.0.2.10:8642"
             "/v1/chat/completions"
         )
 
         variants = (
-            "http://10.10.10.106:8642",
-            "http://10.10.10.106:8642/",
-            "http://10.10.10.106:8642/v1",
-            "http://10.10.10.106:8642/v1/",
+            "http://192.0.2.10:8642",
+            "http://192.0.2.10:8642/",
+            "http://192.0.2.10:8642/v1",
+            "http://192.0.2.10:8642/v1/",
         )
 
         for base_url in variants:
